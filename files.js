c3502f910888ff14180ade5027f71c33f2b3a66b
Azhar Khan <admin@azhark.com>
2022-12-06 14:09:05 +0530

    add read file

--- files.js
+++ files.js
@@ -1,5 +1,11 @@
+const fs = require('fs');
 // reading files
-
+fs.readFile('./docs/notes.txt', (err, data) => {
+    if(err) {
+        console.log(err);
+    }
+    console.log(data.toString());
+});
 
 //writing files
 
