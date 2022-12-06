a2c84a208179a6584292998187e912f7fb5deaf2
Azhar Khan <admin@azhark.com>
2022-12-06 15:02:14 +0530

    add routing

--- server.js
+++ server.js
@@ -1,11 +1,38 @@
+const fs  = require('fs');
 const http = require('http');
 
 const server = http.createServer((req, res) => {
     console.log("request made");
 
     res.setHeader('Content-Type', 'text/html');
-    res.write('<h1>Hello Azhar');
-    res.end();
+    console.log(req.url);
+    path = './views/'
+    switch(req.url) {
+        case '/':
+            path += 'index.html';
+            break;
+        case '/index':
+            path += 'index.html';
+            break;
+        case '/about':
+            path += 'about.html';
+            break;
+        default:
+            path += '404.html';
+            break;
+    }
+    console.log(path);
+    fs.readFile(path, (err, data) => {
+        if(err) {
+            console.log(err);
+            res.end();
+        } else {
+            res.write(data);
+            res.end();
+        }
+
+    })
+
 });
 
 server.listen(3000, 'localhost', () => {
